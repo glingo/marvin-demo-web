cd34499d6c59cd5b3fa50d1056983bf16dccfcb3
glingo <cailly.florian.apple@gmail.com>
2016-10-26 15:46:23 +0200

    clean

--- src/java/demo/DemoWebApplicationInitializer.java
+++ src/java/demo/DemoWebApplicationInitializer.java
@@ -2,6 +2,7 @@ package demo;
 
 import com.marvin.bundle.web.DispatcherServlet;
 import com.marvin.bundle.web.WebApplicationInitializer;
+import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -12,8 +13,10 @@ public class DemoWebApplicationInitializer implements WebApplicationInitializer 
     @Override
     public void onStartup(ServletContext sc) throws ServletException {
         AppKernel kernel = new AppKernel(true);
-        ServletRegistration.Dynamic dispatcher = 
-                sc.addServlet("dispatcher", new DispatcherServlet(kernel));
+        Servlet servlet  = new DispatcherServlet(kernel);
+        
+        ServletRegistration.Dynamic dispatcher = sc.addServlet("dispatcher", servlet);
+        
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/*");
     }
