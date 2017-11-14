7cd32ca45996b7b8f5c5d9007ecb0a548500ab78
Cailly florian <florian.cailly@gmail.com>
2017-11-14 10:54:27 +0100

    rework

--- src/java/marvin/DemoWebApplicationInitializer.java
+++ src/java/marvin/DemoWebApplicationInitializer.java
@@ -6,17 +6,16 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import marvin.app.AppKernel;
+import marvin.demo.app.Application;
 
 public class DemoWebApplicationInitializer implements WebApplicationInitializer {
     
     @Override
     public void onStartup(ServletContext sc) throws ServletException {
-        Servlet servlet  = new FrameworkServlet(new AppKernel("dev", true));
+        Servlet servlet  = new FrameworkServlet(new Application("dev", true));
         
         ServletRegistration.Dynamic registration = sc.addServlet("dispatcher", servlet);
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
-        
     }
 }
